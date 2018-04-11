a1362c9d6c981b33430bc4974f63dd779bbcf4fd
KTK81 <jankotek@gmail.com>
2018-04-11 19:59:24 +0200

    pred skresanim pre mina
    pred skresanim pre mina

--- script.js
+++ script.js
@@ -10,6 +10,14 @@ var korpus = {
     }
 };
 
+var predok = {
+    suflik1:false,
+    suflik1Xstart:780,
+    suflik1Ystart:180,
+    suflik1Xend:1180,
+    suflik1Yend:380
+}
+
 var imageSrcOld1 = "pic\\rea-office-10-10-biela.jpg";
 var imageSrcOld2 = "pic\\rea-office-10-10-biela.jpg";
 var imageSrcOld3 = "pic\\rea-office-10-10-biela.jpg";
@@ -43,20 +51,6 @@ function showChoice (rozmer, farba, predok, TEST) {
     document.getElementById("vyberTEST").style.display = TEST;
 }
 
-// ************************** hlavna funkcia, ktora meni zobrazeny obrazok  ***************************
-//zmeni dany parameter v objecte, nacita z objectu novu verziu suboru k obrazku, prisposobi canvas obrazku, zobrazi novy obrazok
-function changeInfo(vlastnost, hodnota) {
-    korpus[vlastnost]=hodnota;
-    image.src = korpus.obrSubor();
-    canvas.width=image.width;
-    canvas.height=image.height;
-    textName.innerText=image.src;
-    image.onload = function() {
-        ctx.drawImage(image, 0, 0);
-    }
-    //            updateOldImages(imageSRC);
-}
-
 // *        **** ROZMERY *****
 var btnHeight10 = document.getElementById("btnH10");
 var btnHeight20 = document.getElementById("btnH20");
@@ -81,6 +75,58 @@ btnDub.addEventListener("click", function() {changeInfo("farba","dub")});
 btnNavarra.addEventListener("click", function() {changeInfo("farba","navarra")});
 btnWenge.addEventListener("click", function() {changeInfo("farba","wenge")});
 
+// ************************** hlavna funkcia, ktora meni zobrazeny obrazok  ***************************
+//zmeni dany parameter v objecte, nacita z objectu novu verziu suboru k obrazku, prisposobi canvas obrazku, zobrazi novy obrazok
+function changeInfo(vlastnost, hodnota) {
+    korpus[vlastnost]=hodnota;
+    image.src = korpus.obrSubor();
+    canvas.width=image.width;
+    canvas.height=image.height;
+    textName.innerText=image.src;
+    image.onload = function() {
+        ctx.drawImage(image, 0, 0);
+    }
+    //            updateOldImages(imageSRC);
+}
+
+// *****************   PREDOK  ********************
+var btnSuf1 = document.getElementById("suf3");
+btnSuf1.addEventListener("click",function() {drawCanvas()});
+
+function drawCanvas(xStart,yStart, xEnd, yEnd) {
+    var picture = new Image();
+    picture.src = "pic\\suflik-8-1.png";
+    ctx.drawImage(picture, 0, 0, 100, 200);
+//        ctxKorpus.drawImage(picture, 203, 265, 350, 160);
+}
+
+
+
+//*************************************************************************************
+var btnTEST = document.getElementById("spustiTEST");
+btnTEST.addEventListener("click", zistiCoords);
+function zistiCoords(){
+    ctx.canvas.addEventListener('mousemove', function(event){
+        var mouseX = event.layerX;
+        var mouseY = event.layerY;
+        var status = document.getElementById('statusTest');
+        status.innerHTML = mouseX+" | "+mouseY;
+    });
+    ctx.canvas.addEventListener('click', function(event){
+        var mouseX = event.layerX;
+        var mouseY = event.layerY;
+        clickKoordinaty.innerHTML = mouseX+" | "+mouseY;
+    });
+}
+
+
+
+
+
+
+
+
+
 
 //    **************** CANVAS ****************
 //var suflikMaly = document.getElementById("btnSuf10");
@@ -98,59 +144,41 @@ btnWenge.addEventListener("click", function() {changeInfo("farba","wenge")});
 //    
 //}
 
-var btnImgUpload = document.getElementById("imgUpload");
-btnImgUpload.addEventListener("click", uploadCanvasSecond);
+//var btnImgUpload = document.getElementById("imgUpload");
+//btnImgUpload.addEventListener("click", uploadCanvasSecond);
+//
+//function uploadCanvasSecond () {
+//    var canvasSecond = document.getElementById("canvasSecond");
+//    var ctxSecond = canvasSecond.getContext("2d");
+//    var imageKorpus = new Image ();
+//    imageKorpus.src = "pic\\4farby.jpg";
+//    var imageSuflik = new Image ();
+//    imageSuflik.src = "pic\\suflik1.jpg";  
+//    canvasSecond.width=imageKorpus.width;
+//    canvasSecond.height=imageKorpus.height;
+//
+//    ctxSecond.canvas.addEventListener('mousemove', function(event){
+//        var mouseX = event.clientX - ctxSecond.canvas.offsetLeft;
+//        var mouseY = event.clientY - ctxSecond.canvas.offsetTop;
+//        var status = document.getElementById('status');
+//        status.innerHTML = mouseX+" | "+mouseY;
+//    });
+//    ctxSecond.canvas.addEventListener('click', function(event){
+//        var mouseX = event.clientX - ctxSecond.canvas.offsetLeft;
+//        var mouseY = event.clientY - ctxSecond.canvas.offsetTop;
+////         alert(mouseX+" | "+mouseY);
+//        ctx.fillStyle = "orange";
+//        ctx.fillRect(mouseX-15, mouseY-15, 30, 30);
+//    });
+//
+//    imageKorpus.onload = function() {
+//        ctxSecond.drawImage(imageKorpus, 0, 0, canvasSecond.width/2, canvasSecond.height/2);    
+//        ctxSecond.drawImage(imageSuflik, canvasSecond.width/2, canvasSecond.height/2, canvasSecond.width-1, canvasSecond.height-1);    
+//    }
+//    
+//}
 
-function uploadCanvasSecond () {
-    var canvasSecond = document.getElementById("canvasSecond");
-    var ctxSecond = canvasSecond.getContext("2d");
-    var imageKorpus = new Image ();
-    imageKorpus.src = "pic\\4farby.jpg";
-    var imageSuflik = new Image ();
-    imageSuflik.src = "pic\\suflik1.jpg";  
-    canvasSecond.width=imageKorpus.width;
-    canvasSecond.height=imageKorpus.height;
 
-    ctxSecond.canvas.addEventListener('mousemove', function(event){
-        var mouseX = event.clientX - ctxSecond.canvas.offsetLeft;
-        var mouseY = event.clientY - ctxSecond.canvas.offsetTop;
-        var status = document.getElementById('status');
-        status.innerHTML = mouseX+" | "+mouseY;
-    });
-    ctxSecond.canvas.addEventListener('click', function(event){
-        var mouseX = event.clientX - ctxSecond.canvas.offsetLeft;
-        var mouseY = event.clientY - ctxSecond.canvas.offsetTop;
-//         alert(mouseX+" | "+mouseY);
-        ctx.fillStyle = "orange";
-        ctx.fillRect(mouseX-15, mouseY-15, 30, 30);
-    });
-
-    imageKorpus.onload = function() {
-        ctxSecond.drawImage(imageKorpus, 0, 0, canvasSecond.width/2, canvasSecond.height/2);    
-        ctxSecond.drawImage(imageSuflik, canvasSecond.width/2, canvasSecond.height/2, canvasSecond.width-1, canvasSecond.height-1);    
-    }
-    
-}
-
-//*************************************************************************************
-var btnTEST = document.getElementById("spustiTEST");
-btnTEST.addEventListener("click", initCanvas);
-function initCanvas(){
-    var ctx = document.getElementById('canvasTest').getContext('2d');
-    ctx.canvas.addEventListener('mousemove', function(event){
-        var mouseX = event.clientX - ctx.canvas.offsetLeft;
-        var mouseY = event.clientY - ctx.canvas.offsetTop;
-        var status = document.getElementById('statusTest');
-        status.innerHTML = mouseX+" | "+mouseY;
-    });
-    ctx.canvas.addEventListener('click', function(event){
-        var mouseX = event.clientX - ctx.canvas.offsetLeft;
-        var mouseY = event.clientY - ctx.canvas.offsetTop;
-        // alert(mouseX+" | "+mouseY);
-        ctx.fillStyle = "orange";
-        ctx.fillRect(mouseX-15, mouseY-15, 30, 30);
-    });
-}
 
 
 
