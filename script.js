e2e9eb6917e21d0a89b15cc0f34d26dc78ded5d1
KTK81 <jankotek@gmail.com>
2018-04-19 16:27:49 +0200

    peto zmeny

--- script.js
+++ script.js
@@ -4,92 +4,141 @@
 //5.  nacitanie suflikov na 1. click -  picture.onload = function() 
 
 var korpus = {
-    pomocnyNazov:"pic\\korpus-",
-    zaradenie:"NS2",
-    farba:"biela",
-    vyska:"v20",
-    sirka:"s20",
-    hlbka:"h20",
-    obrSubor: function() {
+    pomocnyNazov: "pic\\korpus-",
+    zaradenie: "NS2",
+    farba: "biela",
+    vyska: "v20",
+    sirka: "s20",
+    hlbka: "h20",
+    obrSubor: function () {
         return this.pomocnyNazov + this.vyska + "-" + this.sirka + "-" + this.farba + ".jpg";
     }
 };
 
 var koordinaty = [
-    {zaradenie:"v20", pozicia:2, startX:"130", startY:"160", endX:"588", endY:"368", farba:"siva", zobraz:true},
-    {zaradenie:"v20", pozicia:1, startX:"130", startY:"370", endX:"588", endY:"583", farba:"siva", zobraz:true},
-    {zaradenie:"v30", pozicia:3, startX:"124", startY:"54", endX:"583", endY:"263", farba:"cervena", zobraz:true},
-    {zaradenie:"v30", pozicia:2, startX:"124", startY:"265", endX:"583", endY:"475", farba:"cervena", zobraz:true},
-    {zaradenie:"v30", pozicia:1, startX:"124", startY:"478", endX:"583", endY:"691", farba:"cervena", zobraz:true},
+    {zaradenie: "v20", pozicia: 2, startX: "130", startY: "160", endX: "588", endY: "368", farba: "siva", zobraz: true},
+    {zaradenie: "v20", pozicia: 1, startX: "130", startY: "370", endX: "588", endY: "583", farba: "siva", zobraz: true},
+    {zaradenie: "v30", pozicia: 3, startX: "124", startY: "54", endX: "583", endY: "263", farba: "biela", zobraz: true},
+    {zaradenie: "v30", pozicia: 2, startX: "124", startY: "265", endX: "583", endY: "475", farba: "siva", zobraz: true},
+    {zaradenie: "v30", pozicia: 1, startX: "124", startY: "478", endX: "583", endY: "691", farba: "cervena", zobraz: true},
 ];
 
-
 window.onload = function (e) {
+    init();
     showChoice("block", "block", "block", "none")
-    changeInfo("vyska","v20");
+    changeInfo("vyska", "v20");
 }
 
-//***** horne hlavne menu **** MENU + funkcie na zobrazenie len vybranych <div> elementov
-var menuRozmery = document.getElementById("navRozmery");
-var menuFarba = document.getElementById("navFarba");
-var menuSuflik = document.getElementById("navSuflik");
-var menuCoords = document.getElementById("navCoords");
-var menuAll = document.getElementById("navAll");
+var ctx = null;
+var canvas = null;
+var image = null;
+
+function init() {
+
+    // ************************** hlavna funkcia, ktora meni zobrazeny obrazok  ***************************
+    //zmeni dany parameter v objecte, nacita z objectu novu verziu suboru k obrazku, prisposobi canvas obrazku, zobrazi novy obrazok
+    image = new Image();
+    canvas = document.getElementById("mainPicture");
+    ctx = canvas.getContext('2d');
+
+    //***** horne hlavne menu **** MENU + funkcie na zobrazenie len vybranych <div> elementov
+    var menuRozmery = document.getElementById("navRozmery");
+    var menuFarba = document.getElementById("navFarba");
+    var menuSuflik = document.getElementById("navSuflik");
+    var menuCoords = document.getElementById("navCoords");
+    var menuAll = document.getElementById("navAll");
 
 
-menuRozmery.addEventListener("click", function() { showChoice("block", "none", "none", "none")});
-menuFarba.addEventListener("click", function() { showChoice("none", "block", "none", "none")});
-menuSuflik.addEventListener("click", function() { showChoice("none", "none", "block", "none")}); 
-menuCoords.addEventListener("click", zistiCoords);
-menuAll.addEventListener("click", function() { showChoice("block", "block", "block", "none")});
+    menuRozmery.addEventListener("click", function () {
+        showChoice("block", "none", "none", "none")
+    });
+    menuFarba.addEventListener("click", function () {
+        showChoice("none", "block", "none", "none")
+    });
+    menuSuflik.addEventListener("click", function () {
+        showChoice("none", "none", "block", "none")
+    });
+    menuCoords.addEventListener("click", zistiCoords);
+    menuAll.addEventListener("click", function () {
+        showChoice("block", "block", "block", "none")
+    });
 
-function showChoice (rozmer, farba, predok, TEST) {
+    // *        **** ROZMERY *****
+    var btnHeight10 = document.getElementById("btnH10");
+    var btnHeight20 = document.getElementById("btnH20");
+    btnHeight10.addEventListener("click", function () {
+        changeInfo("vyska", "v20")
+    });
+    btnHeight20.addEventListener("click", function () {
+        changeInfo("vyska", "v30")
+    });
+
+    //       ***** FARBY *****
+    var btnBiela = document.getElementById("btnColorBiela");
+    var btnBuk = document.getElementById("btnColorBuk");
+    var btnDub = document.getElementById("btnColorDub");
+    var btnNavarra = document.getElementById("btnColorNabarra");
+    var btnWenge = document.getElementById("btnColorWenge");
+    btnBiela.addEventListener("click", function () {
+        changeInfo("farba", "biela")
+    });
+    btnBuk.addEventListener("click", function () {
+        changeInfo("farba", "buk")
+    });
+    btnDub.addEventListener("click", function () {
+        changeInfo("farba", "dub")
+    });
+    btnNavarra.addEventListener("click", function () {
+        changeInfo("farba", "navarra")
+    });
+    btnWenge.addEventListener("click", function () {
+        changeInfo("farba", "wenge")
+    });
+
+    // *****************   PREDOK  / SUFLIK / HLAVNY PROBLEM********************
+    var btnSuf3 = document.getElementById("suf3");
+    btnSuf3.addEventListener("click", function () {
+        drawCanvas(3, 124, 54)
+    });
+    var btnSuf2 = document.getElementById("suf2");
+    btnSuf2.addEventListener("click", function () {
+        drawCanvas(2, 124, 265)
+    });
+    var btnSuf1 = document.getElementById("suf1");
+    btnSuf1.addEventListener("click", function () {
+        drawCanvas(1, 124, 478)
+    });
+    //var btnSufB = document.getElementById("sufB");
+    //btnSufB.addEventListener("click",function() {drawCanvas(130, 160)});
+    var btnSufA = document.getElementById("sufA");
+    btnSufA.addEventListener("click", drawPredok);
+
+}
+
+function showChoice(rozmer, farba, predok, TEST) {
     document.getElementById("vyberRozmer").style.display = rozmer;
     document.getElementById("vyberFarba").style.display = farba;
     document.getElementById("vyberPredok").style.display = predok;
     document.getElementById("vyberTEST").style.display = TEST;
 }
 
-// *        **** ROZMERY *****
-var btnHeight10 = document.getElementById("btnH10");
-var btnHeight20 = document.getElementById("btnH20");
-btnHeight10.addEventListener("click", function() {changeInfo("vyska","v20")});
-btnHeight20.addEventListener("click", function() {changeInfo("vyska","v30")});
-
-//       ***** FARBY *****
-var btnBiela = document.getElementById("btnColorBiela");
-var btnBuk = document.getElementById("btnColorBuk");
-var btnDub = document.getElementById("btnColorDub");
-var btnNavarra = document.getElementById("btnColorNabarra");
-var btnWenge = document.getElementById("btnColorWenge");
-btnBiela.addEventListener("click", function() {changeInfo("farba","biela")});
-btnBuk.addEventListener("click", function() {changeInfo("farba","buk")});
-btnDub.addEventListener("click", function() {changeInfo("farba","dub")});
-btnNavarra.addEventListener("click", function() {changeInfo("farba","navarra")});
-btnWenge.addEventListener("click", function() {changeInfo("farba","wenge")});
-
-// ************************** hlavna funkcia, ktora meni zobrazeny obrazok  ***************************
-//zmeni dany parameter v objecte, nacita z objectu novu verziu suboru k obrazku, prisposobi canvas obrazku, zobrazi novy obrazok
-var image = new Image ();
-var canvas = document.getElementById("mainPicture");
-var ctx = canvas.getContext('2d');
 //zmeni hlavny obrazok
 function changeInfo(vlastnost, hodnota) {
-    korpus[vlastnost]=hodnota;
+    korpus[vlastnost] = hodnota;
     image.src = korpus.obrSubor();
-    canvas.width=image.naturalWidth;
-    canvas.height=image.naturalHeight;
 //    canvas.width=image.width;
 //    canvas.height=image.height;
-    textName.innerText=image.src;
-    image.onload = function() {
+    textName.innerText = image.src;
+    image.onload = function () {
+        canvas.width = image.naturalWidth;
+        canvas.height = image.naturalHeight;
         ctx.drawImage(image, 0, 0);
     }
 }
 
-function zistiCoords(){
+function zistiCoords() {
     showChoice("none", "none", "none", "block");
-    ctx.canvas.addEventListener('mousemove', function(event){
+    ctx.canvas.addEventListener('mousemove', function (event) {
         var mouseX = event.layerX;
         var mouseY = event.layerY;
         var mouseXclient = event.clientX;
@@ -98,15 +147,15 @@ function zistiCoords(){
         var mouseYOffset = event.layerY - ctx.canvas.offsetTop;
         var mouseXClientOffset = event.clientX - ctx.canvas.offsetLeft;
         var mouseYClientOffset = event.clientY - ctx.canvas.offsetTop;
-        statusTest.innerHTML = "event.layerX  "+mouseX+" | "+mouseY;
-        statusClient.innerHTML = "event.clientX  "+mouseXclient+" | "+mouseYclient;
-        statusLayerOff.innerHTML = "offset layer "+mouseXOffset+" | "+mouseYOffset;
-        statusClientOff.innerHTML = "offset client "+mouseXClientOffset+" | "+mouseYClientOffset;
+        statusTest.innerHTML = "event.layerX  " + mouseX + " | " + mouseY;
+        statusClient.innerHTML = "event.clientX  " + mouseXclient + " | " + mouseYclient;
+        statusLayerOff.innerHTML = "offset layer " + mouseXOffset + " | " + mouseYOffset;
+        statusClientOff.innerHTML = "offset client " + mouseXClientOffset + " | " + mouseYClientOffset;
     });
-    ctx.canvas.addEventListener('click', function(event){
+    ctx.canvas.addEventListener('click', function (event) {
         var mouseX = event.layerX;
         var mouseY = event.layerY;
-        clickKoordinaty.innerHTML = "event.layerX/Y  "+ mouseX+" | "+mouseY;
+        clickKoordinaty.innerHTML = "event.layerX/Y  " + mouseX + " | " + mouseY;
     });
 //        ctx.canvas.addEventListener('click', function(event){
 //            function search(nameKey, nameKey2) {
@@ -119,46 +168,33 @@ function zistiCoords(){
 //            var test = search("v20", "1");
 //            clickKoordinaty.innerHTML = test.startY;
 //    });
-    
+
 }
-
-
-// *****************   PREDOK  / SUFLIK / HLAVNY PROBLEM********************
-var btnSuf3 = document.getElementById("suf3");
-btnSuf3.addEventListener("click",function() {drawCanvas(3, 124, 54)});
-var btnSuf2 = document.getElementById("suf2");
-btnSuf2.addEventListener("click",function() {drawCanvas(2, 124, 265)});
-var btnSuf1 = document.getElementById("suf1");
-btnSuf1.addEventListener("click",function() {drawCanvas(1, 124, 478)});
-//var btnSufB = document.getElementById("sufB");
-//btnSufB.addEventListener("click",function() {drawCanvas(130, 160)});
-var btnSufA = document.getElementById("sufA");
-btnSufA.addEventListener("click",drawPredok);
 
 function drawCanvas(pozicia, startX, startY) {
     var picture = new Image();
     picture.src = "pic\\suflik-1-biela.png";
-    picture.onload = function() {
+    picture.onload = function () {
         ctx.drawImage(picture, startX, startY);
     }
 }
 
 function drawPredok() {
     var vyska = korpus.vyska;
-    var picture = new Image();
-    picture.src = "pic\\suflik-1-cervena.png";
-    for (var i=0; i<koordinaty.length; i++) {
-        if ((koordinaty[i].zaradenie===vyska)&&(koordinaty[i].zobraz===true)) {
-                                
-            for (var j=1; j<4; j++) {
-                if (koordinaty[i].pozicia===j) {
-                    picture.src = "pic\\suflik-1-"+koordinaty[i].farba+".png";
-                    console.log(picture.src);
-                    var startX = koordinaty[i].startX;
-                    var startY = koordinaty[i].startY;
-//                    picture.onload = function() {
-                        ctx.drawImage(picture, startX, startY);
-//                    }
+    for (var i = 0; i < koordinaty.length; i++) {
+        if ((koordinaty[i].zaradenie === vyska) && (koordinaty[i].zobraz === true)) {
+
+            for (var j = 1; j < 4; j++) {
+                if (koordinaty[i].pozicia === j) {
+                    var picture = new Image();
+                    picture.startX = koordinaty[i].startX;
+                    picture.startY = koordinaty[i].startY;
+                    picture.src = "pic\\suflik-1-" + koordinaty[i].farba + ".png";
+                    picture.onload = function () {
+                        ctx.drawImage(this, this.startX, this.startY);
+                        
+                    }
+
                 }
             }
         }
